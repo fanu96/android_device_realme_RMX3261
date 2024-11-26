ef1d9e0b92c4b12f427b48c9ae81702d180f350a
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-26 04:11:10 +0200

    RMX3261: initial TWRP tree

---
+++ twrp_RMX3261.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from RMX3261 device
+$(call inherit-product, device/realme/RMX3261/device.mk)
+
+PRODUCT_DEVICE := RMX3261
+PRODUCT_NAME := twrp_RMX3261
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3261
+PRODUCT_MANUFACTURER := realme
+
+
