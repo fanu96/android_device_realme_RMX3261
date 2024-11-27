0afa46d89009fda41f3a6a0c872f2b1a3170f421
Ahmed Shahin <83367407+GitFASTBOOT@users.noreply.github.com>
2024-11-27 18:56:30 +0200

    RMX3261: FIX
    *correct brightness path
    *disable system_root

--- TW_flags.mk
+++ TW_flags.mk
@@ -11,7 +11,7 @@ RELAX_USES_LIBRARY_CHECK=true
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/sprd_backlight/backlight/sprd_backlight/brightness"
+TW_BRIGHTNESS_PATH := "/sys/devices/platform/soc/soc:ap-ahb/20400000.dsi/20400000.dsi.0/display/panel0/sprd_backlight/brightness"
 TW_INCLUDE_FASTBOOTD := true
 TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
@@ -23,6 +23,7 @@ TW_NO_BIND_SYSTEM := true
 TW_NO_SCREEN_BLANK := true
 TW_NO_LEGACY_PROPS := true
 TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk"
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 
 # resetprop & repacktools
 TW_INCLUDE_RESETPROP := true
